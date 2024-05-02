e9ee1403fc9465f6f2871ad5dbd754185eb39c55
adianfiuef <103307721+adianfiuef@users.noreply.github.com>
2024-05-02 12:42:43 -0400

    adding functions and a pictures folder

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ class Character{
         this.health = health;
 
     }
+
+    toString(){
+        return `Name: ${this.name}, Attack: ${this.attack}, Defense: ${this.defense}, Health: ${this.health}`;
+        
+    }
+
 }
 
 function randomInt(lower, higher){
@@ -23,6 +29,8 @@ const names = ['Bob', 'Alice', 'John', 'Jane', 'Jack', 'Jill', 'Dylan', 'Megan',
 
 function createCharacters(){
 
+    console.log("create characters")
+
     var name = names[randomInt(0, names.length -1)]
 
     var attack = randomInt(0, 99)
@@ -32,4 +40,30 @@ function createCharacters(){
     var health = randomInt (100, 1000)
 
     characters.push(new Character(name, attack, defense, health));
+}
+
+
+function showCharactersOnPage(){
+
+    console.log("show characters")
+
+    const charactersElement = document.getElementById("character-stats");
+
+    charactersElement.innerHTML = "";
+
+    characters.forEach((character) => {
+
+        const characterDiv = document.createElement(`p`);
+
+        characterDiv.innerHTML = character.toString();
+
+        charactersElement.appendChild(characterDiv);
+
+    });
+
+}
+
+function navigateToImages(){
+    
+    window.location.href = "character-images.html";
 }
